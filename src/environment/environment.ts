d88090d9842283422cbe0d6c599cc6023795f6a9
SantiagoB59 <ferneysantiagobonillacamargo@gmail.com>
2026-07-06 09:47:05 -0500

    campaña de verificacion de alertas enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,13 +1,13 @@
 // src/environments/environment.ts
 
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000'
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000'
+// };
 
 // src/environments/environment.prod.ts
 
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://transmenasmart.com'
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://transmenasmart.com'
+};
